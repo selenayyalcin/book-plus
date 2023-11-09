05d4dc90b175b4f8d160b33e53fe5f74a4130027
selenayyalcin <selenayyalcin@gmail.com>
2023-11-09 15:41:02 +0300

    final continue reading

--- lib/main.dart
+++ lib/main.dart
@@ -11,47 +11,99 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Book+'),
+          backgroundColor: const Color.fromRGBO(45, 115, 109, 1),
         ),
         body: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              // Burada continue reading bölümü olacak
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     const Text(
                       'Continue Reading',
                       style: TextStyle(
-                        fontSize: 24,
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                     const SizedBox(height: 10),
                     Container(
+                      padding: const EdgeInsets.all(4),
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.grey),
                         borderRadius: BorderRadius.circular(8),
                       ),
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Image.network(
-                            'https://img.kitapyurdu.com/v1/getImage/fn:11503783/wh:true/miw:200/mih:200',
-                            width: 150,
-                            height: 200,
-                            fit: BoxFit.cover,
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              'Hayatta Kalanlar',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.network(
+                                    'https://img.kitapyurdu.com/v1/getImage/fn:11503783/wh:true/miw:200/mih:200',
+                                    width: 150,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  const SizedBox(width: 10),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      const SizedBox(
+                                        height: 20,
+                                      ),
+                                      const Text(
+                                        'Hayatta Kalanlar',
+                                        style: TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 5),
+                                      const Text(
+                                        'Alex Schulman',
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                      ),
+                                      const SizedBox(height: 40),
+                                      const SizedBox(
+                                        width: 200,
+                                        height: 20,
+                                        child: LinearProgressIndicator(
+                                          value: 0.4,
+                                          backgroundColor: Colors.grey,
+                                          valueColor:
+                                              AlwaysStoppedAnimation<Color>(
+                                            Color.fromRGBO(45, 115, 109, 1),
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(height: 10),
+                                      ElevatedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor:
+                                                const Color.fromRGBO(
+                                                    45, 115, 109, 1)),
+                                        child: const Text('DID YOU FINISH?'),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
-                            ),
+                            ],
                           ),
                         ],
                       ),
@@ -59,7 +111,7 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
-              // Diğer sayfa içeriği buraya eklenebilir
+              // Diğer sayfa içerikleri
             ],
           ),
         ),
